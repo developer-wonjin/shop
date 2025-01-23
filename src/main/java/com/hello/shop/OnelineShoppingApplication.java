fc8ea6f3970969b8ac7d9a6323f8fe73a9409b1e
wonjin do <wonjin1776@gmail.com>
2025-01-24 00:50:17 +0900

    Initial commit

---
+++ src/main/java/com/hello/shop/OnelineShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.hello.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnelineShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnelineShoppingApplication.class, args);
+	}
+
+}
