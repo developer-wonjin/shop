c338a96b1102e1e198cb07b8530b5912389cd009
wonjin do <wonjin1776@gmail.com>
2025-01-24 10:28:47 +0900

    fix:application.yml등록

--- src/main/java/com/hello/shop/controller/HelloController.java
+++ src/main/java/com/hello/shop/controller/HelloController.java
@@ -1,13 +1,45 @@
 package com.hello.shop.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
+
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String port;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${serverName}")
+    private String serverName;
 
     @GetMapping("/")
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("env", env);
+        responseData.put("port", port);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("serverName", serverName);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+
+
 }
