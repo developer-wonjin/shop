7afdae1f778863db2172b22f7dd88b63acfd7f13
wonjin do <wonjin1776@gmail.com>
2025-01-24 13:06:47 +0900

    fix: 수정

--- src/main/java/com/hello/shop/controller/HelloController.java
+++ src/main/java/com/hello/shop/controller/HelloController.java
@@ -32,6 +32,7 @@ public class HelloController {
         responseData.put("port", port);
         responseData.put("serverAddress", serverAddress);
         responseData.put("serverName", serverName);
+        responseData.put("name", "도원진");
 
         return ResponseEntity.ok(responseData);
     }
